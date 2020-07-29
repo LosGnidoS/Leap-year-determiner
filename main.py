338f320a3f5b5ed539e8b0e3043064ecad16d092
Kirill Shvedov <github.shvedov@gmail.com>
2020-07-30 01:09:31 +0300

    Add files via upload
    This simple script' ll help you to determine the leap year.

---
+++ main.py
@@ -0,0 +1,19 @@
+x = 2020 #year
+
+c = (x / 4) 
+a = (x % 100)
+b = (x % 400)
+
+
+
+while True:
+	if c == int(c):
+		print("OK")
+		break
+		
+	else:
+		if a == 1 and a is b:
+			print("OK")
+		else:
+			print("NO")
+			break
